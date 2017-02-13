1eadec5afdfc2a6be78243725b10a25bd7d47b71
harmless <harmless@syneforge.com>
2017-02-13 18:09:20 +0200

    Cleanup

--- application/classes/Server.class.php
+++ application/classes/Server.class.php
@@ -6,13 +6,24 @@
 
 class Server extends DatabaseObject {
  protected static $table_name = "servers";
- protected static $db_fields = array('serverid', 'addr', 'servername', 'status', 'regdate');
+ protected static $db_fields = array('serverid', 'servername', 'addr', 'status', 'regdate', 'game', 'mode', 'map',
+  'players', 'maxplayers', 'location', 'steam', 'new', 'site', 'about');
 
  public $serverid;
  public $addr;
  public $servername;
  public $status;
  public $regdate;
+ public $game;
+ public $mode;
+ public $map;
+ public $players;
+ public $maxplayers;
+ public $location;
+ public $steam;
+ public $new;
+ public $site;
+ public $about;
 
  public static function add(array $item) {
   $new = new static;
@@ -20,6 +31,16 @@ class Server extends DatabaseObject {
   $new->servername = empty($item['servername']) ? NULL : trim($item['servername']);
   $new->status =  NULL;
   $new->regdate = date("Y-m-d H:i:s");
+  $new->game = empty($item['game']) ? NULL : trim($item['game']);
+  $new->mode = empty($item['mode']) ? NULL : trim($item['mode']);
+  $new->map = NULL;
+  $new->players = NULL;
+  $new->maxplayers = empty($item['maxplayers']) ? NULL : intval(trim($item['maxplayers']));
+  $new->location = empty($item['location']) ? NULL : trim($item['location']);
+  $new->steam = empty($item['steam']) ? NULL : intval(trim($item['steam']));
+  $new->new = 1;
+  $new->site = empty($item['site']) ? NULL : trim($item['site']);
+  $new->about = empty($item['about']) ? NULL : trim($item['about']);
   return $new;
  }
 }
