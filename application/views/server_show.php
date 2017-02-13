31202084cb2263f87b339c4ce0053918373453a7
harmless <harmless@syneforge.com>
2017-02-13 17:47:37 +0200

    Cleanup

---
+++ application/views/server_show.php
@@ -0,0 +1,32 @@
+<?php
+
+echo <<<EOT
+<h1>Server</h1>
+<table id='1table' class='display'>
+<thead>
+ <tr>
+  <th>Param</th>
+  <th>Value</th>
+ </tr>
+</thead>
+<tfoot>
+ <tr>
+  <th>Param</th>
+  <th>Value</th>
+ </tr>
+</tfoot>
+<tbody>
+ <tr>
+  <td>
+   <label for="addr">Address and port</label></td><td><input type="text" value="$data->addr" name="server[addr]"/>
+  </td>
+ </tr>
+ <tr>
+  <td>
+   <label for="servername">Server name</label></td><td><input type="text" value="$data->servername" name="server[servername]"/>
+  </td>
+ </tr>
+ </tbody>
+</table>
+<p><button alt="Edit" title="Edit" onclick="location.href='/users/edit/?userid=$data->userid'">Edit this user</button></p>
+EOT;
