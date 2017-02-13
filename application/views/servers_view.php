27c8c7aa978f6b4fdd2d678c2a479af899a702cd
harmless <harmless@syneforge.com>
2017-02-13 18:19:25 +0200

    Cleanup

--- application/views/servers_view.php
+++ application/views/servers_view.php
@@ -2,6 +2,13 @@
 <p class="form hide">
  <input type="text" value="" placeholder="addr" name="server[addr]"/>
  <input type="text" value="" placeholder="servername" name="server[servername]"/>
+ <input type="text" value="" placeholder="servername" name="server[game]"/>
+ <input type="text" value="" placeholder="servername" name="server[mode]"/>
+ <input type="text" value="" placeholder="servername" name="server[maxplayers]"/>
+ <input type="text" value="" placeholder="servername" name="server[location]"/>
+ <input type="text" value="" placeholder="servername" name="server[steam]"/>
+ <input type="text" value="" placeholder="servername" name="server[site]"/>
+ <input type="text" value="" placeholder="servername" name="server[about]"/>
  <button class="create" title="Create" alt="Create" data-type="server">Create</button>
 </p>
 <p>
@@ -11,10 +18,32 @@
 <thead>
  <th>Address</th>
  <th>Servername</th>
+ <th>Steam</th>
+ <th>Players</th>
+ <th>Maxplayers</th>
+ <th>Map</th>
+ <th>Game</th>
+ <th>Mode</th>
+ <th>Location</th>
+ <th>Registrotion date</th>
+ <th>Site</th>
+ <th>About</th>
+ <th>Status</th>
 </thead>
 <tfoot>
  <th>Address</th>
  <th>Servername</th>
+ <th>Steam</th>
+ <th>Players</th>
+ <th>Maxplayers</th>
+ <th>Map</th>
+ <th>Game</th>
+ <th>Mode</th>
+ <th>Location</th>
+ <th>Registrotion date</th>
+ <th>Site</th>
+ <th>About</th>
+ <th>Status</th>
 </tfoot>
 <tbody>
 <?php
@@ -30,6 +59,17 @@
      <button class="edit" alt="Edit" title="Edit" onclick="location.href='/servers/edit/?serverid=$row->serverid'"></button>
     </span>
    </td>
+   <td>$row->steam</td>
+   <td>$row->players</td>
+   <td>$row->maxplayers</td>
+   <td>$row->map</td>
+   <td>$row->game</td>
+   <td>$row->mode</td>
+   <td>$row->location</td>
+   <td>$row->regdate</td>
+   <td>$row->site</td>
+   <td>$row->about</td>
+   <td>$row->status</td>
   </tr>
 EOT;
  }
