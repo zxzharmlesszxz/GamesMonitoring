6650c069ae02d8139416ca4669727fb8b5b7dc18
harmless <harmless@syneforge.com>
2017-02-14 11:14:08 +0200

    Cleanup

--- application/includes/functions.inc.php
+++ application/includes/functions.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 function games_select_list($servergame = NULL) {
- $str = '<select name="game">';
+ $str = '<select name="server[game]">';
  $str .= (empty($servergame)) ? '<option disabled selected>Select game</option>' : '';
  foreach (config()->games as $game => $title) {
   $str .= "<option value='$game' ".($servergame == $game ? 'selected' : '').">$title</option>";
@@ -11,7 +11,7 @@ function games_select_list($servergame = NULL) {
 }
 
 function modes_select_list($servermode = NULL) {
- $str = '<select name="mode">';
+ $str = '<select name="server[mode]">';
  $str .= (empty($servermode)) ? '<option disabled selected>Select mode</option>' : '';
  foreach (config()->modes as $mode => $title) {
   $str .= "<option value='$mode' ".($servermode == $mode ? 'selected' : '').">$title</option>";
