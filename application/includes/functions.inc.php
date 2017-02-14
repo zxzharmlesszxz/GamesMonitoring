1f898ebfa7f509a4418c4cbb99de3926589d40a4
harmless <harmless@syneforge.com>
2017-02-14 11:09:24 +0200

    Cleanup

--- application/includes/functions.inc.php
+++ application/includes/functions.inc.php
@@ -2,7 +2,7 @@
 
 function games_select_list($servergame = NULL) {
  $str = '<select name="game">';
- $str .= (empty($servergame)) ? '<option disabled selected>Select game</option>' : ''
+ $str .= (empty($servergame)) ? '<option disabled selected>Select game</option>' : '';
  foreach (config()->games as $game => $title) {
   $str .= "<option value='$game' ".($servergame == $game ? 'selected' : '').">$title</option>";
  }
