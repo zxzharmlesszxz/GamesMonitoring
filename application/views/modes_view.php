388eb69f066f14d4d2a1d1a16e74000eccfc6079
harmless <harmless@syneforge.com>
2017-02-24 14:14:12 +0200

    Fix

--- application/views/modes_view.php
+++ application/views/modes_view.php
@@ -1,12 +1,12 @@
 <h1>Servers</h1>
 <p class="form hide">
- <input type="text" value="" placeholder="shortname" name="game[shortname]"/>
- <input type="text" value="" placeholder="fullname" name="game[fullname]"/>
- <input type="description" value="" placeholder="description" name="game[description]"/>
- <button class="create" title="Create" alt="Create" data-type="game">Create</button>
+ <input type="text" value="" placeholder="shortname" name="mode[shortname]"/>
+ <input type="text" value="" placeholder="fullname" name="mode[fullname]"/>
+ <input type="description" value="" placeholder="description" name="mode[description]"/>
+ <button class="create" title="Create" alt="Create" data-type="mode">Create</button>
 </p>
 <p>
- <button alt="Add new game" title="Add new game" id="show">Add new game</button>
+ <button alt="Add new mode" title="Add new mode" id="show">Add new mode</button>
 </p>
 <table id='table' class='display'>
 <thead>
@@ -26,10 +26,10 @@
   $row = $data->getItem($item);
   echo <<<EOT
   <tr>
-   <td><a href="/games/show/?gameid=$row->gameid">$row->fullname</a>
+   <td><a href="/modes/show/?modeid=$row->modeid">$row->fullname</a>
     <span class="actions">
-     <button class="delete" alt="Delete" title="Delete" data-id="$row->gameid" data-type="game"></button>
-     <button class="edit" alt="Edit" title="Edit" onclick="location.href='/games/edit/?gameid=$row->gameid'"></button>
+     <button class="delete" alt="Delete" title="Delete" data-id="$row->modeid" data-type="mode"></button>
+     <button class="edit" alt="Edit" title="Edit" onclick="location.href='/modes/edit/?modeid=$row->modeid'"></button>
     </span>
    </td>
    <td>$row->shortname</td>
