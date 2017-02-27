99820f86ebb15d89cb45ad70722eeee5aeaea8f1
harmless <harmless@syneforge.com>
2017-02-27 12:13:53 +0200

    Comment about column

--- public/application/bootstrap.php
+++ public/application/bootstrap.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once '../private/core/model.php';
-require_once '../private/core/view.php';
-require_once '../private/core/controller.php';
-require_once '../private/core/route.php';
-require_once '../private/includes/functions.inc.php';
+require_once '../../private/core/model.php';
+require_once '../../private/core/view.php';
+require_once '../../private/core/controller.php';
+require_once '../../private/core/route.php';
+require_once '../../private/includes/functions.inc.php';
 
 $config = Config::getInstance();
 Registry::_set('config', $config);
@@ -12,7 +12,7 @@ $database = new MySQL_Database;
 Registry::_set('database', $database);
 
 function __autoload($class){
- @include_once __DIR__."/../private/classes/${class}.class.php";
+ @include_once __DIR__."/../../private/classes/${class}.class.php";
 }
 
 function config(){
