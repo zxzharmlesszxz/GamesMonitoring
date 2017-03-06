2f6b411e9cbd26da91b2c631970c72eb13a2eaca
Harmless <harmless@syneforge.com>
2017-03-06 14:32:26 +0200

    Add setStatus

--- application/models/model_cron.php
+++ application/models/model_cron.php
@@ -19,7 +19,7 @@ class Model_Cron extends Model {
    $sq->connect($address[0], $address[1]);
    $item->info = $sq->getInfo();
    if (empty($item->info)) {
-    $item->status = 0;
+    $item->setStatus(0);
    }
    $item->players_info = $sq->getPlayers();
    $item->rules = $sq->getRules();
