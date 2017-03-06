199119d7327006bb72905480b2a4fd200c99c0ae
Harmless <harmless@syneforge.com>
2017-03-06 16:38:32 +0200

    Cleanup

--- application/classes/Countries.class.php
+++ application/classes/Countries.class.php
@@ -1,6 +1,5 @@
 ﻿<?php
 class Countries {
- protected $matches = Array();
  protected $countries = Array(
   "ua" => "Україна",
   "us" => "USA",
@@ -253,32 +252,4 @@ class Countries {
   "jp" => "Япония",
   "ru" => "Россия",
  );
-  
- public function makeSelect($qresult, $total_option = true) {
-  $this->getMatches($qresult);
-  global $servers_total;
-  $result = "<select name='country'>";
-  if($total_option) $result .= "<option value='all' selected>Все страны </option>";
-  foreach($this->countries as $key => $name) {
-   if(isset($this->matches[$key])) {
-    $cur_matches = $this->matches[$key];
-   } else {
-    $cur_matches = 0;
-   }
-   
-   if($cur_matches != 0) $result .= "<option value='$key'>$name </option>\n";
-  }
-  $result .= "</select>";
-  echo $result;
- }
-
- public function getMatches($qresult) {
-  while($server = dbarray_fetch($qresult)) {
-    if(empty($matches[$server['server_location']])) {
-     $this->matches[strtolower($server['server_location'])] = 1;
-    } else {
-     $this->matches[strtolower($server['server_location'])]++;
-    }
-  }
- }
 }
