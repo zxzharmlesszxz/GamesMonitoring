8eb2328325a71b035442c9c3abf99167efb97742
Harmless <harmless@syneforge.com>
2017-03-09 16:16:42 +0200

    debug

--- application/controllers/controller_login.php
+++ application/controllers/controller_login.php
@@ -1,25 +1,31 @@
 <?php
 
 class Controller_Login extends Controller{
-	function action_index(){
-		//$data["login_status"] = "";
-		if(isset($_POST['login']) && isset($_POST['password'])){
-			$login = $_POST['login'];
-			$password =$_POST['password'];
-			if($login=="admin" && $password=="12345"){
-				$data["login_status"] = "access_granted";
-				session_start(); echo $_SESSION['admin'];
-				$_SESSION['admin'] = $password;
-				header('Location: /admin/');
-			}else{
-				$data["login_status"] = "access_denied";
-			}
-		}else{
-			$data["login_status"] = "";
-		}
+ protected $query;
 
-		$data['debug'] = $_REQUEST;
+ public function __construct() {
+  parent::__construct();
+  $this->model = new Model_Admins();
+  $this->query = $_REQUEST;
+ }
 
-		$this->view->generate('login_view.php', 'template_view.php', $data);
-	}
+ public function action_index(){
+   if ($login=="admin" && $password=="12345"){
+    $data["login_status"] = "access_granted";
+    session_start();
+    echo $_SESSION['admin'];
+    $_SESSION['admin'] = $password;
+    header('Location: /admin/');
+   } else {
+    $data["login_status"] = "access_denied";
+   }
+  } else {
+   $data["login_status"] = "";
+  }
+
+  $data['debug']['query'] = $this->query;
+  $data['debug']['model'] = $this->model;
+
+  $this->view->generate('login_view.php', 'template_view.php', $data);
+ }
 }
