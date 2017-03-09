a62f3326a886d2ff2f66dea02e6560f8f7e4af72
Harmless <harmless@syneforge.com>
2017-03-09 17:28:27 +0200

    debug

--- application/classes/SourceServerQueries.class.php
+++ application/classes/SourceServerQueries.class.php
@@ -4,13 +4,12 @@
  */
 final class SourceServerQueries extends ServerQueries {
  public function getInfo() {
-  if( !$this->connected )
-   return false;
+  if (!$this->connected) return false;
   $return = array();
   $this->send("\xFF\xFF\xFF\xFFTSource Engine Query\x00");
-  if($tmp = $this->read(true)) {
-  #if($tmp = $this->read()) { // if server don't use dproto
-   if($tmp == 0x6d) {
+  #if ($tmp = $this->read(true)) {
+  if ($tmp = $this->read()) { // if server don't use dproto
+   if ($tmp == 0x6d) {
     $this->getString();
     $return['serverName'] = $this->getString();
     $return['mapName'] = $this->getString();
@@ -23,14 +22,14 @@ final class SourceServerQueries extends ServerQueries {
     $tmp = chr($this->getByte());
     $return['operatingSystem'] = ($tmp == 'l') ? 'Linux' : 'Windows';
     $return['passwordProtected'] = ($this->getByte() == 0x01) ? true : false;
-    if($this->getByte() == 0x01) {
+    if ($this->getByte() == 0x01) {
      $this->getString();
      $this->getString();
      $this->raw = substr($this->raw, 11);
     }
     $return['secureServer'] = ($this->getByte() == 0x01) ? true : false;
     $return['botNumber'] = $this->getByte();
-   } elseif($tmp == 0x49) {
+   } elseif ($tmp == 0x49) {
     $return['version'] = $this->getByte();
     $return['serverName'] = $this->getString();
     $return['mapName'] = $this->getString();
@@ -55,20 +54,19 @@ final class SourceServerQueries extends ServerQueries {
 
  function getPlayers() {
   $return = array();
-  if(!$this->connected)
-   return $return;
+  if (!$this->connected) return $return;
   $this->send("\xFF\xFF\xFF\xFF\x55\xFF\xFF\xFF\xFF");
   $tmp = $this->read();
-  if($tmp == 0x41) {
+  if ($tmp == 0x41) {
    $this->send("\xFF\xFF\xFF\xFF\x55" . $this->raw);
    $tmp = $this->read();
-  } elseif(!$tmp) {
+  } elseif (!$tmp) {
    $this->send("\xFF\xFF\xFF\xFF\x55" . $this->getChallenge());
    $tmp = $this->read();
   }
   if($tmp == 0x44) {
    $num = $this->getByte();
-   for($i = 0; $i < $num; $i++ ) {
+   for ($i = 0; $i < $num; $i++) {
     $tmp = $this->getByte();
     $name = $this->getString();
     $kills = $this->getLong();
@@ -85,53 +83,51 @@ final class SourceServerQueries extends ServerQueries {
 
  function getRules() {
   $return = array();
-  if(!$this->connected)
-   return $return;
+  if (!$this->connected) return $return;
   $this->send("\xFF\xFF\xFF\xFF\x56\xFF\xFF\xFF\xFF");
   $tmp = $this->read();
-  if($tmp == 0x41) {
+  if ($tmp == 0x41) {
    $this->send("\xFF\xFF\xFF\xFF\x56" . $this->raw);
    $tmp = $this->read();
-  } elseif(!$tmp) {
+  } elseif (!$tmp) {
    $this->send("\xFF\xFF\xFF\xFF\x56" . $this->getChallenge());
    $tmp = $this->read();
   }
-  if($tmp == 0x45) {
+  if ($tmp == 0x45) {
    $num = $this->getShort();
-   for($i=0; $i<$num; $i++) {
+   for ($i=0; $i<$num; $i++) {
     $name = $this->getString();
     $value = $this->getString();
-    if($name)
-     $return[$name] = $value;
+    if($name) $return[$name] = $value;
    }
   }
   return $return;
  }
 
  private function getChallenge() {
-  if( !$this->connected )
-   return false;
+  if (!$this->connected) return false;
   $this->send("\xFF\xFF\xFF\xFF\x57");
   $this->read();
   return substr($this->raw, 5);
  }
  
  protected function read($many_packets = false) {
-  if($many_packets) {
+  if ($many_packets) {
    parent::read(true);
    $this->getLong();
   } else {
    parent::read();
-   if($this->getLong() == -2) {
+   if ($this->getLong() == -2) {
     $requestId = $this->getLong();
     $pacets = $this->getByte();
     $tmp = $this->getLong();
-    if($requestId < 0) {
+    if ($requestId < 0) {
      $this->getLong();
      $this->getByte();
      $this->raw = substr(bzdecompress($this->raw), 4);
-    } elseif(($tmp < 0) && (substr($this->raw, 0, 3) == "\xFF\xFF\xFF"))
+    } elseif (($tmp < 0) && (substr($this->raw, 0, 3) == "\xFF\xFF\xFF")) {
      $this->raw = substr($this->raw, 3);
+    }
     $tmp = $this->raw;
     for ($i = 1; $i < $pacets; $i++) {
      parent::read();
