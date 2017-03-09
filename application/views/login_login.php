f5e9f52b158d873fe4ac0ce9a15edcb7c62f8f18
Harmless <harmless@syneforge.com>
2017-03-09 17:01:50 +0200

    debug

--- application/views/login_login.php
+++ application/views/login_login.php
@@ -1,7 +1,7 @@
 <h1>Страница авторизации</h1>
-<?php if($login_status=="access_granted") { ?>
+<?php if($data['login_status']=="access_granted") { ?>
 <p style="color:green">Авторизация прошла успешно.</p>
-<?php } elseif($login_status=="access_denied") { ?>
+<?php } elseif($data['login_status']=="access_denied") { ?>
 <p style="color:red">Логин и/или пароль введены неверно.</p>
 <?php } ?>
 <?php
