5fda1ddf3486b1533c3b6952f51c7215f2ad1e80
Harmless <harmless@syneforge.com>
2017-03-09 16:45:39 +0200

    debug

---
+++ application/views/login_login.php
@@ -0,0 +1,9 @@
+<h1>Страница авторизации</h1>
+<?php extract($data); ?>
+<?php if($login_status=="access_granted") { ?>
+<p style="color:green">Авторизация прошла успешно.</p>
+<?php } elseif($login_status=="access_denied") { ?>
+<p style="color:red">Логин и/или пароль введены неверно.</p>
+<?php } ?>
+<?php
+var_dump($data);
