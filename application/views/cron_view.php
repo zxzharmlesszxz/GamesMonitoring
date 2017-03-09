c0a3fb71b481d68608012ca41b21fa2bfdb96a32
Harmless <harmless@syneforge.com>
2017-03-09 12:23:43 +0200

    Added new futures and cleanup

--- application/views/cron_view.php
+++ application/views/cron_view.php
@@ -6,10 +6,15 @@
  <th>Address</th>
  <th>Steam</th>
  <th>Players</th>
+ <th>Bots</th>
  <th>Maxplayers</th>
  <th>Map</th>
  <th>Game</th>
  <th>Mode</th>
+ <th>Password protected</th>
+ <th>Secure</th>
+ <th>OS</th>
+ <th>Protocol</th>
  <th>Status</th>
 </thead>
 <tfoot>
@@ -18,10 +23,15 @@
  <th>Address</th>
  <th>Steam</th>
  <th>Players</th>
+ <th>Bots</th>
  <th>Maxplayers</th>
  <th>Map</th>
  <th>Game</th>
  <th>Mode</th>
+ <th>Password protected</th>
+ <th>Secure</th>
+ <th>OS</th>
+ <th>Protocol</th>
  <th>Status</th>
 </tfoot>
 <tbody>
@@ -36,10 +46,15 @@
    <td>$row->addr</td>
    <td>$row->steam</td>
    <td>$row->players</td>
+   <td>$row->botNumber</td>
    <td>$row->maxplayers</td>
    <td>$row->map</td>
    <td>$row->game</td>
    <td>$row->mode</td>
+   <td>$row->passwordProtected</td>
+   <td>$row->secureServer</td>
+   <td>$row->operatingSystem</td>
+   <td>$row->version</td>
    <td>$row->status</td>
   </tr>
 EOT;
@@ -48,9 +63,3 @@ EOT;
 ?>
  </tbody>
 </table>
-
-<?php
-foreach($data->keys() as $item){
- $row = $data->getItem($item);
- var_dump($row->info);
-}
