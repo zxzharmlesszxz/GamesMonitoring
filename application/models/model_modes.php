a5c55d024784b6e3027ad263e74f3b0049182504
Harmless <harmless@syneforge.com>
2017-03-10 17:51:08 +0200

    Cleanup

--- application/models/model_modes.php
+++ application/models/model_modes.php
@@ -4,7 +4,7 @@ class Model_Modes extends Model {
  public function getAjax() {
   $array = array();
   foreach ($this->items->getItems() as $key => $item) {
-   $array[$key] = get_object_vars($item);
+   $array[$key] = json_encode($item);
   }
   return $array;
  }
