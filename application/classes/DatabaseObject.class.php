ed37a497c7ef53a4a857344b105540d9adc41b6f
Harmless <harmless@syneforge.com>
2017-03-10 11:59:44 +0200

    Debug

--- application/classes/DatabaseObject.class.php
+++ application/classes/DatabaseObject.class.php
@@ -158,4 +158,12 @@ abstract class DatabaseObject {
         db()->query($sql);
         return (db()->affected_rows() == 1) ? true : false;
     }
+
+    public function __sleep() {
+     return $this->attributes();
+    }
+
+    public function __toArray() {
+     return $this->__sleep();
+    }
 }
