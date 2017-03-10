ee1e611705af4d38dd06f90ee7fcb231ded2555f
Harmless <harmless@syneforge.com>
2017-03-10 18:10:11 +0200

    Cleanup

--- application/models/model_modes.php
+++ application/models/model_modes.php
@@ -5,6 +5,7 @@ class Model_Modes extends Model {
   $array = array();
   foreach ($this->items->getItems() as $key => $item) {
    $array[$key] = json_encode($item);
+   $array[json_encode($item)];
   }
   return $array;
  }
