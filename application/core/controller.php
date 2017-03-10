c76c2c4346b05944601215626317d6409b286095
Harmless <harmless@syneforge.com>
2017-03-10 14:00:10 +0200

    Cleanup

--- application/core/controller.php
+++ application/core/controller.php
@@ -15,7 +15,7 @@ abstract class Controller {
  public function __construct() {
   $this->view = new View();
   $model = "Model_".explode("_", static::class)[1];
-  $this->model = new $model();
+  $this->model = (class_exists($model)) ? new $model() : '';
   $this->query = $_REQUEST;
  }
 
