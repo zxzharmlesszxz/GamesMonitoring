b9103f55377a7f754f8064e7b0eb05f295d68ff6
Harmless <harmless@syneforge.com>
2017-03-13 18:50:57 +0200

    Implement ajax function

--- application/models/model_servers.php
+++ application/models/model_servers.php
@@ -14,4 +14,30 @@ class Model_Servers extends Model {
   }
   return $old->save() ? $old : false;
  }
+
+ public function getAjax() {
+  $items = parent::getAjax()['data'];
+  $result = array();
+  foreach ($items as $id => $item) {
+   $result[] = array(
+    'serverid'=> $item->serverid,
+    'servername' => $item->servername,
+    'addr' => $item->addr,
+    'steam' => $item->steam,
+    'maxplayers' => $item->maxplayers,
+    'players' => $item->players,
+    'map' => $item->map,
+    'game' => $item->game,
+    'mode' => $item->mode,
+    'location' => $item->location,
+    'botNumber' => $item->botNumber,
+    'regdate' => $item->regdate,
+    'site' => $item->site,
+    'vip' => $item->vip,
+    'top' => $item->top,
+    'status' => $item->status,
+   );
+  }
+  return array('data' => $result);
+ }
 }
