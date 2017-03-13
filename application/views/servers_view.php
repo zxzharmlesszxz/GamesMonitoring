50eb2e3ef076b603684a5212491daa20a3f8b0cc
Harmless <harmless@syneforge.com>
2017-03-13 18:44:43 +0200

    Implement ajax function

--- application/views/servers_view.php
+++ application/views/servers_view.php
@@ -12,42 +12,96 @@
 <p>
  <button alt="Add new server" title="Add new server" id="show">Add new server</button>
 </p>
-<table id='table' class='display'>
+<script type="text/javascript">
+ $(document).ready(function() {
+  $('#table1').DataTable({
+   "processing": true,
+   "ajax": {
+    "url": "/servers/getAll/",
+    "dataSrc": function (json) {
+      var return_data = [];
+//      json.data.forEach(function(item){alert(item);});
+      for(var i=0;i< json.data.length; i++){
+        var item = json.data[i];
+        return_data.push({
+          'server': '<a href="/servers/show/?serverid='+item.serverid+'">'+item.servername+'</a>'+
+            '<span class="actions">'+
+            '<button class="delete" title="Delete" data-id="'+item.serverid+'" data-type="server"></button>'+
+            '<button class="edit" title="Edit" onclick="location.href=\'/servers/edit/?serverid='+item.serverid+'\'"></button>'+
+            '</span>',
+          'addr': item.addr,
+          'steam': item.steam,
+          'players': item.players+"/"+item.botNumber+"/"+item.maxplayers,
+          'map': item.map,
+          'game': item.game,
+          'mode': item.mode,
+          'location': item.location,
+          'regdate': item.regdate,
+          'site': item.site,
+          'status': '<input class="status" type="checkbox" data-id="'+item.serverid+'" value="'+item.status+'" data-type="server" disabled readonly />',
+          'vip': '<input class="vip" type="checkbox" data-id="'+item.serverid+'" value="'+item.vip+'" data-type="server" disabled readonly />',
+          'top': item.top,
+        })
+      }
+      return return_data;
+    }
+   },
+   "columns": [
+    { "data": "server"},
+    { "data": "addr"},
+    { "data": "steam" },
+    { "data": "players" },
+    { "data": "map" },
+    { "data": "game" },
+    { "data": "mode" },
+    { "data": "location" },
+    { "data": "regdate" },
+    { "data": "site" },
+    { "data": "status" },
+    { "data": "vip" },
+    { "data": "top" },
+   ]
+  });
+ });
+</script>
+<table id='table1' class='display'>
 <thead>
- <th>Servername</th>
- <th>Address</th>
- <th>Steam</th>
- <th>Players/Bots/Maxplayers</th>
- <th>Map</th>
- <th>Game</th>
- <th>Mode</th>
- <th>Location</th>
- <th>Registration date</th>
- <th>Site</th>
- <!--<th>About</th>-->
- <th>Status</th>
- <th>VIP</th>
- <th>TOP</th>
+ <tr>
+  <th>Servername</th>
+  <th>Address</th>
+  <th>Steam</th>
+  <th>Players/Bots/Maxplayers</th>
+  <th>Map</th>
+  <th>Game</th>
+  <th>Mode</th>
+  <th>Location</th>
+  <th>Registration date</th>
+  <th>Site</th>
+  <th>Status</th>
+  <th>VIP</th>
+  <th>TOP</th>
+ </tr>
 </thead>
 <tfoot>
- <th>Servername</th>
- <th>Address</th>
- <th>Steam</th>
- <th>Players/Bots/Maxplayers</th>
- <th>Map</th>
- <th>Game</th>
- <th>Mode</th>
- <th>Location</th>
- <th>Registration date</th>
- <th>Site</th>
- <!--<th>About</th>-->
- <th>Status</th>
- <th>VIP</th>
- <th>TOP</th>
+ <tr>
+  <th>Servername</th>
+  <th>Address</th>
+  <th>Steam</th>
+  <th>Players/Bots/Maxplayers</th>
+  <th>Map</th>
+  <th>Game</th>
+  <th>Mode</th>
+  <th>Location</th>
+  <th>Registration date</th>
+  <th>Site</th>
+  <th>Status</th>
+  <th>VIP</th>
+  <th>TOP</th>
+ </tr>
 </tfoot>
 <tbody>
 <?php
-
+/*
  foreach($data->keys() as $item){
   $row = $data->getItem($item);
   echo <<<EOT
@@ -75,7 +129,7 @@
   </tr>
 EOT;
  }
-
+*/
 ?>
  </tbody>
 </table>
