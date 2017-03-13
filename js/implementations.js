d037da5aeee0224cb08d20698463ec041fa6c1cd
harmless <harmless@syneforge.com>
2017-03-13 02:39:54 +0200

    Cleanup

--- js/implementations.js
+++ js/implementations.js
@@ -2,13 +2,6 @@ $(document).ajaxComplete(function(){
   // Autocheck checkbox if value = 1
   $('input[type="checkbox"]').each(function(){ ($(this).val() == 1) ? $(this).prop('checked', true) : $(this).prop('checked', false); });
 
-  // Colored nav menu
-  $('nav#menu').children('menu').children('li').each(function(){
-    if ($(this).children('a').attr('href') == location.pathname){
-      $(this).toggleClass('active');
-    }
-  });
-
   // display map image in table
     var list = document.querySelectorAll("td[data-icon]");
     for (var i = 0; i < list.length; i++) {
