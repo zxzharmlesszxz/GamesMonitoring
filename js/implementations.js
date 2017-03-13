1004eb507190aa11b941c14e2196b83ad2694428
harmless <harmless@syneforge.com>
2017-03-13 22:04:39 +0200

    Fix

--- js/implementations.js
+++ js/implementations.js
@@ -3,7 +3,7 @@ $(document).ajaxComplete(function(){
   $('input[type="checkbox"]').each(function(){ ($(this).val() == 1) ? $(this).prop('checked', true) : $(this).prop('checked', false); });
 
   // display map image in table
-    var list = document.querySelectorAll("div[data-icon]");
+    var list = document.querySelectorAll("div.map[data-icon]");
     for (var i = 0; i < list.length; i++) {
      var url = list[i].getAttribute('data-icon');
      list[i].style.backgroundImage = "url('" + url + "')";
