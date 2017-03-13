bab717343893a0ef5fb434a79408472af5d52348
harmless <harmless@syneforge.com>
2017-03-13 02:37:54 +0200

    Cleanup

--- js/implementations.js
+++ js/implementations.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ajaxComplete(function(){
   // Autocheck checkbox if value = 1
   $('input[type="checkbox"]').each(function(){ ($(this).val() == 1) ? $(this).prop('checked', true) : $(this).prop('checked', false); });
 
