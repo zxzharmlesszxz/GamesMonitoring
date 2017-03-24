6ce231ee6776af8e4e08ea3811b8c16b6f415eaf
Harmless <harmless@syneforge.com>
2017-03-24 11:32:18 +0200

    Fixed login

--- application/views/admin_login.php
+++ application/views/admin_login.php
@@ -22,13 +22,5 @@ var_dump(session());
   </table>
  </form>
 </p>
-
-
 <?php
-extract($data);
-if ($login_status=="access_granted") { ?>
-<p style="color:green">Авторизация прошла успешно.</p>
-<?php } elseif($login_status=="access_denied") { ?>
-<p style="color:red">Логин и/или пароль введены неверно.</p>
-<?php }
 var_dump($data);
