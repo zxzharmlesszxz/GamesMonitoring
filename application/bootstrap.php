86c083dc0470d914a595ae205124ef62c382f9d5
Harmless <harmless@syneforge.com>
2017-05-04 18:52:03 +0300

    Refactor

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -1,9 +1,10 @@
 <?php
 
+namespace Core;
 require_once 'Core/index.php';
 require_once 'includes/functions.inc.php';
 
-Core\Registry::_set('config', Core\Config::getInstance());
+Registry::_set('config', Config::getInstance());
 #Registry::_set('database', new MySQL_Database);
 #Registry::_set('session', new Session);
 
@@ -22,21 +23,21 @@ function __autoload($class)
  * @return mixed|null
  */
 function config(){
- return Core\Registry::_get('config');
+ return Registry::_get('config');
 }
 
 /**
  * @return mixed|null
  */
 function db(){
- return Core\Registry::_get('database');
+ return Registry::_get('database');
 }
 
 /**
  * @return mixed|null
  */
 function session() {
- return Core\Registry::_get('session');
+ return Registry::_get('session');
 }
 
-Core\Router::start();
+Router::start();
