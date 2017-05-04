3c7e1c657081b6c81d0ef926a42e5edb4dbd1a85
Harmless <harmless@syneforge.com>
2017-05-04 12:01:51 +0300

    Simple fixes

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once __DIR__ . 'core/model.php';
-require_once __DIR__ . 'core/view.php';
-require_once __DIR__ . 'core/controller.php';
-require_once __DIR__ . 'core/route.php';
-require_once __DIR__ . 'includes/functions.inc.php';
+require_once __DIR__ . '/core/model.php';
+require_once __DIR__ . '/core/view.php';
+require_once __DIR__ . '/core/controller.php';
+require_once __DIR__ . '/core/route.php';
+require_once __DIR__ . '/includes/functions.inc.php';
 
 Registry::_set('config', Config::getInstance());
 Registry::_set('database', new MySQL_Database);
