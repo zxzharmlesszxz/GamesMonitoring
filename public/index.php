718e8f2d30cef5985eb7741501a6c87db3bce530
Harmless <harmless@syneforge.com>
2017-05-04 17:45:55 +0300

    Refactor

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 ini_set('display_errors', 1);
 try {
-    require_once __DIR__ . '../application/bootstrap.php';
+    require_once __DIR__ . '/../application/bootstrap.php';
 } catch (Exception $e) {
     echo "Error: ", $e->getMessage(), "\n";
 }
