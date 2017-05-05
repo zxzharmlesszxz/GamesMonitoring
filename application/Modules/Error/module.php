501023c9f48d9c08a60249f56d626a9e65e42ad8
Harmless <harmless@syneforge.com>
2017-05-05 14:08:47 +0300

    Refactor

--- application/Modules/Error/module.php
+++ application/Modules/Error/module.php
@@ -6,21 +6,35 @@
  * Time: 17:37
  */
 
-namespace Module;
+namespace Module\Error;
 use Core;
 require_once 'Controller/controller.php';
 
-class Error implements Core\Module
+/**
+ * Class Module
+ * @package Module\Error
+ */
+class Module implements Core\Module
 {
+    /**
+     * @var Controller
+     */
     public $Controller;
 
+    /**
+     * Module constructor.
+     */
     public function __construct()
     {
-        $this->Controller = new Controller();
+        $this->Controller = new Module\Error\Controller();
     }
 }
 
-spl_autoload_register(function ($class) {
+spl_autoload_register(
+/**
+ * @param $class
+ */
+    function ($class) {
     $hierarchy = explode('\\', $class);
     @include_once __DIR__ . '/Class/' . end($hierarchy) . '.php';
 });
