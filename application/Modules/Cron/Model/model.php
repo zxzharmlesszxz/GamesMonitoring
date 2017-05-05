fb7c7b205ae850c5753668e2e63d2fdd89218086
Harmless <harmless@syneforge.com>
2017-05-05 18:59:07 +0300

    Refactor

--- application/Modules/Cron/Model/model.php
+++ application/Modules/Cron/Model/model.php
@@ -3,25 +3,29 @@
 namespace Module\Cron;
 use Core\Interfaces\ModelInterface;
 
+/**
+ * Class Model
+ * @package Module\Cron
+ */
 class Model implements ModelInterface
 {
-    public function __construct()
-    {
-
-    }
-
-    public function get_data()
-    {
-        return $this->cron_update();
-    }
-
-    protected function cron_update()
-    {
-
-    }
-
+    /**
+     *
+     */
     public function create() {}
+
+    /**
+     *
+     */
     public function get() {}
+
+    /**
+     *
+     */
     public function delete() {}
+
+    /**
+     *
+     */
     public function save() {}
 }
--- application/Modules/Game/Model/model.php
+++ application/Modules/Game/Model/model.php
@@ -4,42 +4,29 @@ namespace Module\Game;
 
 use Core\Interfaces\ModelInterface;
 
+/**
+ * Class Model
+ * @package Module\Game
+ */
 class Model implements ModelInterface
 {
     /**
-     * @param array $item
-     * @return bool|mixed
+     *
      */
-    public function update(array $item)
-    {
-        $item = array_map("trim", $item);
-
-        $old = $this->items->getItem($item['gameid']);
-        unset($item['gameid']);
-        if (!$old) {
-            return FALSE;
-        }
-        foreach ($item as $key => $value) {
-            $old->$key = $value;
-        }
-        return $old->save() ? $old : false;
-    }
+    public function create() {}
 
     /**
-     * @return array
+     *
      */
-    public function getAjax()
-    {
-        $items = parent::getAjax()['data'];
-        $result = array();
-        foreach ($items as $id => $item) {
-            $result[] = array(
-                'gameid' => $item->gameid,
-                'fullname' => $item->fullname,
-                'shortname' => $item->shortname,
-                'description' => $item->description
-            );
-        }
-        return array('data' => $result);
-    }
+    public function get() {}
+
+    /**
+     *
+     */
+    public function delete() {}
+
+    /**
+     *
+     */
+    public function save() {}
 }
