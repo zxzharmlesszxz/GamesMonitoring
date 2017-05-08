f1c5a7ef99c0f1af84718155a1958b675036e0a5
Harmless <harmless@syneforge.com>
2017-05-08 12:19:58 +0300

    Refactor

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -26,9 +26,9 @@ while (false !== ($module = $modulesDir->read()))
 var_dump($core);
 var_dump($core->CoreModules);
 var_dump($core->getCoreModule('Error'));
-#var_dump($core->Modules);
-#var_dump($core->getModule('Admin'));
-#var_dump(config());
+var_dump($core->Modules);
+var_dump($core->getModule('Admin'));
+var_dump(config());
 
 /**
  * @param $class
