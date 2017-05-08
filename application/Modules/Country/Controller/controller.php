422e6c7aad510312a6a89da5205e818e93c78dff
Harmless <harmless@syneforge.com>
2017-05-08 11:10:17 +0300

    Refactor

---
+++ application/Modules/Country/Controller/controller.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Module\Country;
+
+use Core\Interfaces;
+
+/**
+ * Class Controller
+ * @package Module\Country
+ */
+class Controller implements Interfaces\ControllerInterface
+{
+    /**
+     * Controller constructor.
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * @param string $action
+     * @return mixed|void
+     */
+    public function action($action = 'index')
+    {
+
+    }
+}
---
+++ application/Modules/Country/Model/model.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Module\Country;
+use Core\Interfaces;
+
+/**
+ * Class Model
+ * @package Module\Country
+ */
+class Model implements Interfaces\ModelInterface
+{
+    /**
+     *
+     */
+    public function create() {}
+
+    /**
+     *
+     */
+    public function get() {}
+
+    /**
+     *
+     */
+    public function delete() {}
+
+    /**
+     *
+     */
+    public function save() {}
+}
