bd0f2adada4873ce439fbc41c4e5a249f910a643
Harmless <harmless@syneforge.com>
2017-05-08 11:58:19 +0300

    Refactor

---
+++ application/Core/Module/Database/Controller/controller.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Core\Module\Database;
+use Core\Interfaces;
+
+
+/**
+ * Class Controller
+ * @package Core\Module\Database
+ */
+class Controller implements Interfaces\ControllerInterface
+{
+    /**
+     * Controller constructor.
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * @param string $action
+     * @return mixed|void
+     */
+    public function action($action = 'index')
+    {
+
+    }
+}
--- application/Core/Module/Database/module.php
+++ application/Core/Module/Database/module.php
@@ -6,20 +6,37 @@
  * Time: 17:37
  */
 
-namespace Module;
-use Core;
+namespace Core\Module;
 
-class Database implements Core\Module
+use Core\Interfaces;
+
+require_once 'Controller/controller.php';
+
+/**
+ * Class Database
+ * @package Core\Module\Database
+ */
+class Database implements Interfaces\ModuleInterface
 {
+    /**
+     * @var Database\Controller
+     */
     public $Controller;
 
+    /**
+     * Database constructor.
+     */
     public function __construct()
     {
-        $this->Controller = new Controller();
+        $this->Controller = new Database\Controller();
     }
 }
 
-spl_autoload_register(function ($class) {
-    $hierarchy = explode('\\', $class);
-    @include_once __DIR__ . '/Class/' . end($hierarchy) . '.php';
-});
+spl_autoload_register(
+/**
+ * @param $class
+ */
+    function ($class) {
+        $hierarchy = explode('\\', $class);
+        @include_once __DIR__ . '/Class/' . end($hierarchy) . '.php';
+    });
