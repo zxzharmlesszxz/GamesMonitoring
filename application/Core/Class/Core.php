81772fc5105fb686eb9b1b807b8b0155f3a6b833
Harmless <harmless@syneforge.com>
2017-05-10 15:40:36 +0300

    Refactor

--- application/Core/Class/Core.php
+++ application/Core/Class/Core.php
@@ -73,7 +73,7 @@ class Core
         var_dump(is_null($this->Config->THEME));
         var_dump(!is_null($this->Config->THEME) ? $this->Config->THEME : $this->Config->DEFAULT_THEME);
         var_dump((!is_null($this->Config->THEME) ? $this->Config->THEME : $this->Config->DEFAULT_THEME));
-        $this->Theme = new Theme((is_null($this->Config->THEME) ? $this->Config->DEFAULT_THEME : $this->Config->DEFAULT_THEME));
+        $this->Theme = new Theme((!is_null($this->Config->THEME) ? $this->Config->THEME : $this->Config->DEFAULT_THEME));
     }
 
     /**
