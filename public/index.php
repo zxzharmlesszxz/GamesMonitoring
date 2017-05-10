bb62649b9c635ddb19ccd7e3b84a68f5f531d13e
Harmless <harmless@syneforge.com>
2017-05-10 15:15:59 +0300

    Refactor

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ ini_set('display_errors', 1);
 try {
     require_once __DIR__ . '/../application/bootstrap.php';
     var_dump($_SESSION);
+    var_dump($core->Config->THEME);
 } catch (Exception $e) {
     echo "Error: ", $e->getMessage(), "\n";
 }
