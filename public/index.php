6960f6c2d2ce482eda75a93e567bad656497f262
Harmless <harmless@syneforge.com>
2017-05-11 11:06:30 +0300

    Refactor

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 ini_set('display_errors', 1);
 try {
-    require_once __DIR__ . '/../application/bootstrap.php';
+    var_dump(require_once __DIR__ . '/../application/bootstrap.php');
     var_dump($_SESSION);
     var_dump($core);
 } catch (Exception $e) {
