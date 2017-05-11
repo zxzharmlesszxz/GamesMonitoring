d371e74764a22a4e398bde1804ed522431009ef3
Harmless <harmless@syneforge.com>
2017-05-11 12:21:39 +0300

    Refactor

--- application/Core/Class/Core.php
+++ application/Core/Class/Core.php
@@ -56,7 +56,7 @@ class Core implements SingletonInterface
      */
     private function __construct()
     {
-        #$this->Config = Config::getInstance();
+        $this->Config = Config::getInstance();
         #$this->Router = Router::getInstance();
         $this->Modules = new Collection();
         $this->CoreModules = new Collection();
