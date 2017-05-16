24865dfe7fd5f11ed404a73ff44cd81e8d6ed9ce
Harmless <harmless@syneforge.com>
2017-05-16 14:32:16 +0300

    Refactor

--- application/Core/Class/Core.php
+++ application/Core/Class/Core.php
@@ -139,7 +139,7 @@ class Core implements SingletonInterface
                     break;
                 default:
                     var_dump($this->Config->PROJECT_ROOT . '/' . $this->Config->CORE_MODULE_PATH . '/' . $module . '/module.php');
-                    include_once $this->Config->PROJECT_ROOT . '/' . $this->Config->CORE_MODULE_PATH . '/' . $module . '/module.php';
+                    var_dump(include_once $this->Config->PROJECT_ROOT . '/' . $this->Config->CORE_MODULE_PATH . '/' . $module . '/module.php');
                     $moduleName = "Core\\Module\\$module";
                     var_dump($moduleName);
                     var_dump(class_exists($moduleName));
