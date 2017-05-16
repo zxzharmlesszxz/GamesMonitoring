38a5d96fffc737405d07d285fba0e0c7ad671936
Harmless <harmless@syneforge.com>
2017-05-16 14:13:25 +0300

    Refactor

--- application/Core/Class/Core.php
+++ application/Core/Class/Core.php
@@ -60,7 +60,7 @@ class Core implements SingletonInterface
         $this->Router = Router::getInstance();
         $this->Modules = new Collection();
         $this->CoreModules = new Collection();
-        $this->loadCoreModules();
+        //$this->loadCoreModules();
         $this->Session = $this->getCoreModule('Session');
         $this->Theme = $this->getTheme();
     }
