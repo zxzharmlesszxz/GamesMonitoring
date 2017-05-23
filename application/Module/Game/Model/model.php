afc558af2c7553afe44a6709319c7f1501c092e3
Harmless <harmless@syneforge.com>
2017-05-23 18:26:17 +0300

    Refactor

--- application/Module/Game/Model/model.php
+++ application/Module/Game/Model/model.php
@@ -19,7 +19,10 @@ class Model extends \Core\Model
     public function get()
     {
         $template = file_get_contents(__DIR__ . '/../View/games_view.php');
-        $content = Game::find_all();
+        $content = "";
+        foreach (Game::find_all() as $item) {
+            $content .= "<tr><td>$item->fullname</td><td>$item->shortname</td><td>$item->description</td></tr>\n";
+        }
         return str_replace('%content%', $content, $template);
     }
 }
