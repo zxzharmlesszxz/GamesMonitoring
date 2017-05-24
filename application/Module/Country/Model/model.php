cd13dccb949ccb12a7d8897f3b05db5223c934f0
Harmless <harmless@syneforge.com>
2017-05-24 13:25:46 +0300

    Refactor

--- application/Module/Country/Model/model.php
+++ application/Module/Country/Model/model.php
@@ -11,7 +11,7 @@ class Model extends \Core\Model
     public function get()
     {
         echo __METHOD__;
-        var_dump(Country::count_all());
+        var_dump(new Country);
         $template = file_get_contents(__DIR__ . '/../View/countries_view.php');
         $content = "";
         foreach (Country::find_all() as $item) {
