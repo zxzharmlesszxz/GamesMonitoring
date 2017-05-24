bc8ae0ef617430936a36e4e4ea533ceebb545661
Harmless <harmless@syneforge.com>
2017-05-24 15:04:35 +0300

    Refactor

--- Application/Core/Class/Theme.php
+++ Application/Core/Class/Theme.php
@@ -129,7 +129,7 @@ abstract class Theme implements ThemeInterface
     {
         $path = dir($this->dir . '/' . $dir);
         $parsedpath = explode('/', $this->dir);
-        $webpath = "/application/Theme/" . end($parsedpath);
+        $webpath = "/Application/Theme/" . end($parsedpath);
         $files = array();
         while (false !== ($file = $path->read())) {
             switch ($file) {
--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 try {
-    require_once __DIR__ . '/../application/bootstrap.php';
+    require_once __DIR__ . '/../Application/bootstrap.php';
 } catch (Exception $e) {
     echo "Error: ", $e->getMessage(), "\n";
 }
