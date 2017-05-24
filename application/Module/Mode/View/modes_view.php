1b7b8fb9af3d5565df39124dc96981eb4de857aa
Harmless <harmless@syneforge.com>
2017-05-24 11:25:44 +0300

    Refactor

--- application/Module/Mode/View/modes_view.php
+++ application/Module/Mode/View/modes_view.php
@@ -8,37 +8,6 @@
 <p>
  <button title="Add new mode" id="show">Add new mode</button>
 </p>
-<script type="text/javascript">
- $(document).ready(function() {
-  $('#table').DataTable({
-   "processing": true,
-   "ajax": {
-    "url": "/modes/getAll/",
-    "dataSrc": function (json) {
-      var return_data = [];
-      for(var i=0;i< json.data.length; i++){
-        var item = json.data[i];
-        return_data.push({
-          'mode': '<a href="/modes/show/?modeid='+item.modeid+'">'+item.fullname+'</a>'+
-            '<span class="actions">'+
-            '<button class="delete" title="Delete" data-id="'+item.modeid+'" data-type="mode"></button>'+
-            '<button class="edit" title="Edit" onclick="location.href=\'/modes/edit/?modeid='+item.modeid+'\'"></button>'+
-            '</span>',
-          'shortname': item.shortname,
-          'description': item.description
-        })
-      }
-      return return_data;
-    }
-   },
-   "columns": [
-    { "data": "mode"},
-    { "data": "shortname"},
-    { "data": "description" }
-   ]
-  });
- });
-</script>
 <table id='table' class='display'>
  <thead>
   <tr>
