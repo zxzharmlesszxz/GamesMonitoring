833ac4238612c97e2f6690e27cb37676094e24bf
Harmless <harmless@syneforge.com>
2017-05-25 14:16:29 +0300

    Refactor

--- Application/Core/Module/Menu/Controller/controller.php
+++ Application/Core/Module/Menu/Controller/controller.php
@@ -10,6 +10,6 @@ class Controller extends \Core\Controller
 {
     public function add()
     {
-        $this->Model->add(func_get_args());
+        $this->Model->add(func_get_arg(0), func_get_arg(1), func_get_arg(2),func_get_arg(3));
     }
 }
