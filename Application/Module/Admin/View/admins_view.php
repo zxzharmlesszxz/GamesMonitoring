490c02f11c08ecc0c7b032ba634917abec915a09
Harmless <harmless@syneforge.com>
2017-05-29 12:23:35 +0300

    Refactor

--- Application/Module/Admin/View/admins_view.php
+++ Application/Module/Admin/View/admins_view.php
@@ -9,6 +9,39 @@
 <p>
     <button alt="Add new Admin" title="Add new Admin" id="show">Add new Admin</button>
 </p>
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#table').DataTable({
+            "processing": true,
+            "ajax": {
+                "url": "/admins/get/?ajax=true",
+                "dataSrc": function (json) {
+                    var return_data = [];
+                    for (var i = 0; i < json.data.length; i++) {
+                        var item = json.data[i];
+                        return_data.push({
+                            'admin': '<a href="/admins/show/?login=' + item.login + '">' + item.login + '</a>' +
+                            '<span class="actions">' +
+                            '<button class="delete" title="Delete" data-id="' + item.adminid + '" data-type="admin"></button>' +
+                            '<button class="edit" title="Edit" onclick="location.href=\'/admins/edit/?adminid=' + item.adminid + '\'"></button>' +
+                            '</span>',
+                            'username': item.username,
+                            'email': item.email,
+                            'status': '<input class="status" type="checkbox" data-id="' + item.adminid + '" value="' + item.status + '" data-type="admin" />'
+                        })
+                    }
+                    return return_data;
+                }
+            },
+            "columns": [
+                {"data": "admin"},
+                {"data": "username"},
+                {"data": "email"},
+                {"data": "status"}
+            ]
+        });
+    });
+</script>
 <p>
 <table id='table' class='display'>
     <thead>
@@ -28,7 +61,6 @@
     </tr>
     </tfoot>
     <tbody>
-    %content%
     </tbody>
 </table>
 </p>
