e1d2ff3e365d60b3b6037371f40c4ea851266676
Harmless <harmless@syneforge.com>
2017-05-29 17:43:11 +0300

    Refactor

--- Application/Module/Admin/module.php
+++ Application/Module/Admin/module.php
@@ -27,13 +27,14 @@ class Admin extends Module
         global $core;
         parent::__construct();
         $this->addRoute('login');
-        if ($core->getCoreModule('Session')->Session->get('type') == 'admin') {
+        var_dump($core->getCoreModule('Session'));
+        //if ($core->getCoreModule('Session')->Session->get('type') == 'admin') {
             $this->addRoute('delete');
             $this->addRoute('create');
             $this->addRoute('save');
             $this->addRoute('logout');
             $this->addRoute('changeStatus');
-        }
+        //}
     }
 }
 
