d923f9bb0b8a947c6f62e4abb30f29b406d32752
Harmless <harmless@syneforge.com>
2017-05-29 17:29:15 +0300

    Refactor

--- Application/Module/Admin/Model/model.php
+++ Application/Module/Admin/Model/model.php
@@ -100,7 +100,7 @@ class Model extends \Core\Model
     {
         $query = func_get_arg(0)->getQuery()['id'];
         $new = Admin::find_by_id($query);
-        var_dump($new);
+        var_dump($query);
         return $new->changeStatus();
     }
 }
