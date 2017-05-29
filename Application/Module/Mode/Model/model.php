f26546b5aaf29364a734e1da04d084cecb40d78a
Harmless <harmless@syneforge.com>
2017-05-29 12:58:32 +0300

    Refactor

--- Application/Module/Mode/Model/model.php
+++ Application/Module/Mode/Model/model.php
@@ -8,13 +8,35 @@ namespace Module\Mode;
  */
 class Model extends \Core\Model
 {
-    public function get()
+    protected function ajax(array $items)
     {
-        $template = file_get_contents(__DIR__ . '/../View/modes_view.php');
+        $content = array();
+        foreach ($items as $item) {
+            $content['data'][] = array('id' => $item->id, 'fullname' => $item->fullname, 'shortname' => $item->shortname, 'description' => $item->description);
+        }
+        return $content;
+    }
+
+    protected function str(array $items)
+    {
         $content = "";
-        foreach (Mode::find_all() as $item) {
+        foreach ($items as $item) {
             $content .= "<tr><td>$item->fullname</td><td>$item->shortname</td><td>$item->description</td></tr>\n";
         }
-        return str_replace('%content%', $content, $template);
+        return $content;
+    }
+
+    /**
+     * @return array|mixed|string
+     */
+    public function get()
+    {
+        $query = func_get_arg(0)->getQuery();
+        $template = file_get_contents(__DIR__ . '/../View/modes_view.php');
+        if (isset($query['ajax']) and $query['ajax'] == true) {
+            return $this->ajax(Mode::find_all());
+        } else {
+            return str_replace('%content%', $this->str(Mode::find_all()), $template);
+        }
     }
 }
