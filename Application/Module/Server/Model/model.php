147ffea17ea1c6654ea555260b1152fcb142c163
Harmless <harmless@syneforge.com>
2017-05-30 11:21:32 +0300

    Refactor

--- Application/Module/Server/Model/model.php
+++ Application/Module/Server/Model/model.php
@@ -49,19 +49,19 @@ class Model extends \Core\Model
         $server->players_info = $sq->getPlayers();
         $server->rules = $sq->getRules();
         $sq->disconnect();
-        $content = str_replace('%serverName%', $server->servername, $template);
-        $content = str_replace('%id%', $server->id, $template);
-        $content = str_replace('%address%', $server->addr, $template);
-        $content = str_replace('%game%', $server->game, $template);
-        $content = str_replace('%map%', $server->map, $template);
-        $content = str_replace('%version%', $server->version, $template);
-        $content = str_replace('%secureServer%', $server->secureServer, $template);
-        $content = str_replace('%passwordProtected%', $server->passwordProtected, $template);
-        $content = str_replace('%operatingSystem%', $server->operatingSystem, $template);
-        $content = str_replace('%about%', $server->about, $template);
-        var_dump($content);
-        var_dump($server);
-        return $content;
+        $template = str_replace('%serverName%', $server->servername, $template);
+        $template = str_replace('%id%', $server->id, $template);
+        $template = str_replace('%address%', $server->addr, $template);
+        $template = str_replace('%game%', $server->game, $template);
+        $template = str_replace('%map%', $server->map, $template);
+        $template = str_replace('%version%', $server->version, $template);
+        $template = str_replace('%secureServer%', $server->secureServer, $template);
+        $template = str_replace('%passwordProtected%', $server->passwordProtected, $template);
+        $template = str_replace('%operatingSystem%', $server->operatingSystem, $template);
+        $template = str_replace('%about%', $server->about, $template);
+        //var_dump($content);
+        //var_dump($server);
+        return $template;
     }
 
     /**
