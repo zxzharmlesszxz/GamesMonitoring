ff678aab1c235c4dd24cd16114803f14ba444164
Harmless <harmless@syneforge.com>
2017-06-08 15:58:59 +0300

    Refactor

--- Application/Module/Server/View/servers_view.php
+++ Application/Module/Server/View/servers_view.php
@@ -61,13 +61,15 @@
                 {"data": "top"}
             ]
         });
-    });
-    $('#table').on('page.dt', function () {
-        var list = document.querySelectorAll("div.map[data-icon]");
-        for (var i = 0; i < list.length; i++) {
-            var url = list[i].getAttribute('data-icon');
-            list[i].style.backgroundImage = "url('" + url + "')";
-        }
+
+
+        $('#table').on('page.dt', function () {
+            var list = document.querySelectorAll("div.map[data-icon]");
+            for (var i = 0; i < list.length; i++) {
+                var url = list[i].getAttribute('data-icon');
+                list[i].style.backgroundImage = "url('" + url + "')";
+            }
+        });
     });
 </script>
 <p>
